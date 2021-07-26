c2c53d6ac68e51a1d433e674868e077c9b9a6a39
Himanshu <rathihimanshu@live.com>
2021-07-27 00:13:08 +0530

    Create azuretf.tf

---
+++ azuretf.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.65"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "myTFResourceGroup"
+  location = "westus2"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+    name                = "myTFVnet"
+    address_space       = ["10.0.0.0/16"]
+    location            = "westus2"
+    resource_group_name = azurerm_resource_group.rg.name
+}
+Copy
